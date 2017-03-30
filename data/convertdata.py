4a0e72b0484ca5aaae006dd9227ea83c73d8a513
Xing <xli227@uic.edu>
2017-03-30 16:46:18 -0500

    added all converted mat files as well as test.html to view graphs individually

--- data/convertdata.py
+++ data/convertdata.py
@@ -1,18 +1,35 @@
 import scipy.io as sio
 import numpy as np
+import glob 
 
-## set the threshold
-threshold = 0.01
 
-data = sio.loadmat('Connect_DZ_M1.mat')
-## print(np.min(np.nonzero(data['DZ_M'])))
-data_array = data['DZ_M']
-low_values_indices = data_array < threshold  # find low value indices
-print(low_values_indices)
-data_array[low_values_indices] = 0
+def convertData(threshold, flag):
+    for files in glob.glob("./Connect_*"):
+        ##print(files)
+        data = sio.loadmat(files)
+        for key in data:
+            if key in ['DZ_M','DZ_N', 'DZ_R', 'HC_M', 'HC_N', 'HC_R']:
+                data_array = data[key]
+        ##print(data_array)
+        low_values_indices = data_array < threshold  # find low value indices
+        #print(low_values_indices)
+        data_array[low_values_indices] = 0
+        
+        #flag to zero out number opposite the diagonal since it's an undirected graph
+        if flag == True:
+            row = data_array.shape[0]
+            col = data_array.shape[1]
+            for i in range(0, row):
+                for j in range(0, col):
+                    if j < i:
+                        data_array[i,j] = 0
+        #normalize array
+        normalized = data_array/np.amin(data_array[np.nonzero(data_array)])*10
+        trun = np.trunc(normalized)
+        ##print(trun)
+        ##print(files[2:-4])
+        np.savetxt('json/'+files[2:-4]+'.json', trun, fmt='%i', delimiter=',')
 
-#normalize array
-normalized = data_array/np.amin(data_array[np.nonzero(data_array)])
-trun = np.trunc(normalized)
-print(trun)
-np.savetxt('test.json', trun, fmt='%i', delimiter=',')
+if __name__ == "__main__":
+    threshold = 0.02341644
+    convertData(threshold, False)
