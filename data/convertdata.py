34e4297221fc837d29de5b700ed8046c02299217
Xing <xli227@uic.edu>
2017-03-30 21:46:30 -0500

    added side by side comparisons for chord diagrams

--- data/convertdata.py
+++ data/convertdata.py
@@ -9,11 +9,23 @@ def convertData(threshold, flag):
             threshold(float): sets the threshold the data will be filtered to
             flag (Boolean): determine whether or not the values across the diagonal will be zeroed out
     '''
+    ##intialize counters
+    keys = ['DZ_M','DZ_N', 'DZ_R', 'HC_M', 'HC_N', 'HC_R']
+    count = {}
+    for key in keys:
+        count[key] = 0
+
     for files in glob.glob("./Connect_*"):
-        ##print(files)
+        file_key = ''
+        for key in keys:
+            if files.find(key) > 0:
+                file_key = key
+  
+        count[file_key] += 1
+
         data = sio.loadmat(files)
         for key in data:
-            if key in ['DZ_M','DZ_N', 'DZ_R', 'HC_M', 'HC_N', 'HC_R']:
+            if key in keys:
                 data_array = data[key]
         ##print(data_array)
         low_values_indices = data_array < threshold  # find low value indices
@@ -31,7 +43,7 @@ def convertData(threshold, flag):
         #normalize array
         normalized = data_array/np.amin(data_array[np.nonzero(data_array)])*10
         trun = np.trunc(normalized)
-        np.savetxt('json/'+files[2:-4]+'.json', trun, fmt='%i', delimiter=',')
+        np.savetxt('json/' + files[2:14] + str(count[file_key]) +'.json', trun, fmt='%i', delimiter=',')
 
 if __name__ == "__main__":
     threshold = 0.02341644
