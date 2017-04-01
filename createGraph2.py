042d1ec1817c055401e16a48b5b8d6a29ed49f95
ahuang27 <ahuang27@uic.edu>
2017-04-01 01:17:48 -0500

    remaining to do list in this file

--- createGraph2.py
+++ createGraph2.py
@@ -1,3 +1,9 @@
+#This file computes most of the graph properties we said we were going to investigate.
+
+#The only thing missing is that it does not yet generate the output files for the global properties, and the path-related properties.
+#THIS IS LEFT TO BE DONE.
+#Once this is generated, statistical tests also need to be done on the rest of the global properties.
+
 # This file was prepared by consulting the documentation for networkx online.
 
 import scipy.io as sio
@@ -58,11 +64,11 @@ def __init__():
         labelToIndexMap[tup[1]] = it
         it+=1
 
-    print(labelToIndexMap)
-    lst = ["N", "M", "R"]
     channels = ["FCz", "Fz", "T7", "T8", "FC2", "FC6", "Iz", "P7"]
 
     outframes = [pandas.DataFrame() for k in range(0, len(channels))]
+
+    lst = ["N", "M", "R"]
     
     for i in lst:
         tup1 = createGraph("DZ_" + i)
@@ -74,6 +80,7 @@ def __init__():
     
 def createGraph(path):
     G =[] # list of graphs for each file
+    G_inv = []
     n = 0
     arr = np.ndarray(34)
     dirs = "C:/Anaconda3/Connect_"
@@ -96,18 +103,28 @@ def createGraph(path):
         for key in dictionary: #there is only one k-v pair, so this is just 1 iteration
             arr = dictionary[key]
         G.append(nx.Graph())
+        G_inv.append(nx.Graph())
 
         G[n].add_nodes_from(range(0, 33))
+        G_inv[n].add_nodes_from(range(0,33))
 
         for i in range(0,34):
             for j in range(i +1, 34):
                 if arr[i][j] >= threshold:
                     G[n].add_edge(i, j, weight = arr[i][j])
+                    #convert weights to a second, inverse version
+                    G_inv[n].add_edge(i,j,weight=1/(arr[i][j]))
 
+        #Average Clustering Coefficient
+        avg = nx.average_clustering(G[n])
+
+        for g in nx.connected_component_subgraphs(G_inv[n]):
+            if len(g.nodes()) > 1:
+            print(nx.average_shortest_path_length(g))
+            
         deg_cent_dict = nx.degree_centrality(G[n])
                 # returns a dictionary
 
-        print(deg_cent_dict)
         lstFCzDegs.append(deg_cent_dict[labelToIndexMap["'FCz'"]])
         lstFzDegs.append(deg_cent_dict[labelToIndexMap["'Fz'"]])
         lstT7Degs.append(deg_cent_dict[labelToIndexMap["'T7'"]])
@@ -120,9 +137,10 @@ def createGraph(path):
             lstIzDegs.append(0)
         lstP7Degs.append(deg_cent_dict[labelToIndexMap["'P7'"]])
         
+        #LEFT TO BE DONE: extract properties for select nodes from above from the below dictionaries
+        bet_cent_dict = nx.betweenness_centrality(G_inv[n])
+        close = nx.closeness_centrality(G_inv[n])
 
-        #have to convert weights to a second, inverse version to perform the next function call
-        #bet_cent_dict = nx.betweenness_centrality(G[n])
 
         n+=1
 
